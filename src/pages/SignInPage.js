baa8454a00e6e857e0f4167efe9bccc8da3b0d50
Roberta Capalbo <robertacapalbo@hotmail.com>
2023-04-23 14:27:45 -0300

    singup & signin

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,14 +1,36 @@
 import styled from "styled-components"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import MyWalletLogo from "../components/MyWalletLogo"
+import { useContext} from "react"
+import axios from "axios"
+import usDt from "../context/usDt"
+import usCtx from '../context/usCtx';
+
 
 export default function SignInPage() {
+
+  const navigate = useNavigate();
+  const loginInfos = {
+    email,
+    password
+  }
+  const {setToken} = useContext(usDt)
+  let {email, setEmail, password, setPassword, setUsername} = useContext(usCtx);
+  
+  function handleForm(retorno){
+    retorno.preventDefault();
+    const promise = axios.post('http://localhost:5000/sign-in', loginInfos)
+    promise.then(res => {setToken(res.data); navigate("/home"); setEmail('');
+    setPassword('');setUsername(res.data.username);})
+    promise.catch(err => {console.log(err)})}
+  
+
   return (
     <SingInContainer>
-      <form>
+      <form onSubmit={{handleForm}}>
         <MyWalletLogo />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
+        <input placeholder="E-mail" type="email" required onChange={(retorno) => setEmail(retorno.target.value)} value={email}/>
+        <input placeholder="Senha" type="password" autocomplete="new-password" name='password' required onChange={(retorno) => setPassword(retorno.target.value)} value={password}/>
         <button>Entrar</button>
       </form>
 
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,16 +1,39 @@
-import { Link } from "react-router-dom"
 import styled from "styled-components"
 import MyWalletLogo from "../components/MyWalletLogo"
+import styled from "styled-components"
+import { Link, useNavigate } from "react-router-dom"
+import { useContext} from "react"
+import axios from "axios"
+import usCtx from '../context/usCtx';
 
 export default function SignUpPage() {
+
+  const navigate = useNavigate();
+  const {email, setEmail, password, setPassword, name, setName, confirmPassword, setconfirmPassword} = useContext(usCtx);
+  const signupInfos = {
+      name,
+      email,
+      password,
+      confirmPassword
+  }
+
+  function handleForm(retorno){
+    retorno.preventDefault();
+    const promise = axios.post('http://localhost:5000/sign-up', signupInfos)
+    promise.then(res => {setName(''); navigate('/home'); setEmail(''); setPassword(''); setconfirmPassword('');})
+    promise.catch(err => {console.log(err)});
+}
+
+  
+
   return (
     <SingUpContainer>
-      <form>
+      <form onSubmit={handleForm}>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
+        <input placeholder="Nome" type="text" name='name' required value={name} onChange={(retorno) => setName(retorno.target.value)}/>
+        <input placeholder="E-mail" type="email" name='email' required value={email} onChange={(retorno) => setEmail(retorno.target.value)}/>
+        <input placeholder="Senha" type="password" autocomplete="new-password" name='password' required value={password} onChange={(retorno) => setPassword(retorno.target.value)}/>
+        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" name='password' required  value={confirmPassword} onChange={(retorno) => setconfirmPassword(retorno.target.value)}/>
         <button>Cadastrar</button>
       </form>
 
